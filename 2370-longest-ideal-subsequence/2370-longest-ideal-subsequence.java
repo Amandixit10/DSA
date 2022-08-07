f641ce999e875dd616273f97c147177f635fad99
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-08-07 14:13:34 +0530

    Time: 58 ms (57.14%), Space: 47.9 MB (85.71%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.java
@@ -1,24 +1,22 @@
 class Solution {
-    Integer dp[][];
     public int longestIdealString(String s, int k) {
-        dp=new Integer[s.length()][27];
-        return help(s,k,-1,0);
-    }
-    int help(String s,int k ,int prev,int idx)
-    {
-        if(idx>=s.length())
-        {return 0;}
-        if(dp[idx][prev+1]!=null)
-        {return dp[idx][prev+1];}
-        char ch=s.charAt(idx);
-        int num=ch-'a';
-        int a=0;
-        if(Math.abs(num-prev)<=k||prev==-1)
+        int dp[]=new int[26];
+        int n=s.length();
+        int omax=0;
+        for(int i=0;i<n;i++)
         {
-         a=1+help(s,k,num,idx+1);   
+        int val=s.charAt(i)-'a';
+            int max=0;
+            for(int j=0;j<26;j++)
+            { 
+                if(Math.abs(j-val)<=k)
+                {
+                    max=Math.max(max,1+dp[j]);
+                }
+            }
+            dp[val]=max;
+            omax=Math.max(max,omax);
         }
-        a=Math.max(a,help(s,k,prev,idx+1));
-        dp[idx][prev+1]=a;
-        return dp[idx][prev+1];
+        return omax;
     }
 }
