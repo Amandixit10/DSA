111deef23cd2fc82e3006226aa605502bc30483f
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-08-11 20:35:21 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (88.84%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -15,22 +15,18 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
+        boolean left=validate(root.left,root.val,Long.MIN_VALUE);
+        boolean right=validate(root.right,Long.MAX_VALUE,root.val);
+        return left&right;
+    }
+    boolean validate(TreeNode root,long max,long min)
+    {
         if(root==null)
         {return true;}
-    return isValidBST(root.left)&isValidBST(root.right)&(help(root.left,0,root.val))&(help(root.right,1,root.val));
+        if(root.val>=max||root.val<=min)
+        {return false;}
+        boolean left=validate(root.left,root.val,min);
+        boolean right=validate(root.right,max,root.val);
+        return left&right;
     }
-    boolean help(TreeNode node,int flag,int val)
-    {
-        if(node==null)
-        {return true;}
-        if(flag==0)
-        {
-         if(node.val>=val)
-         {return false;}
-            return help(node.left,flag,val)&help(node.right,flag,val);
-        }
-                   if(node.val<=val)
-         {return false;}
-            return help(node.left,flag,val)&help(node.right,flag,val);  
-        }
 }
