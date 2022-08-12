571421046c251835e531e8f99169de1e5874dfe9
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-08-12 17:09:41 +0530

    Time: 233 ms (80.83%), Space: 196.7 MB (56.48%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.java
+++ 2080-range-frequency-queries/2080-range-frequency-queries.java
@@ -1,67 +1,15 @@
 class RangeFreqQuery {
-HashMap<Integer,ArrayList<Integer>> map;
-    public RangeFreqQuery(int[] arr) {
-        map=new HashMap<>();
-        for(int i:arr)
-        {
-            ArrayList<Integer> list=new ArrayList<>();
-            map.put(i,list);
-        }
-        for(int i=0;i<arr.length;i++)
-        {
-            ArrayList<Integer> list=map.get(arr[i]);
-            list.add(i);
-            map.put(arr[i],list);
-        }
-    }
-    
-    public int query(int left, int right, int value) {
-    
-        ArrayList<Integer> list=map.get(value);
-        if(!map.containsKey(value)||list.get(0)>right||list.get(list.size()-1)<left)
-        {return 0;}
-        int a=0;
-        int b=0;
-        int hi=list.size();
-        int lo=-1;
-        while(hi-lo>1)
-        {
-            int mid=lo+(hi-lo)/2;
-            if(list.get(mid)<=right)
-            {
-                lo=mid;
-            }
-            else{
-                hi=mid;
-            }
-        }
-        a=lo;
-        hi=list.size();
-        lo=-1;
-            while(hi-lo>1)
-        {
-            int mid=lo+(hi-lo)/2;
-            if(list.get(mid)>=left)
-            {
-                hi=mid;
-            }
-            else{
-                lo=mid;
-            }
-        }
-        b=hi;
-        if(a==-1&&b==list.size())
-        {return list.size();}
-        else if(a==-1)
-        {
-            return b+1;
-        }
-        else if(b==list.size())
-        {
-            return list.size()-a;
-        }
-return a-b+1;
-    }
+List<Integer>[] ids = new ArrayList[10001];
+Function<Integer, Integer> lower_bound = (pos) -> pos < 0 ? ~pos : pos;
+public RangeFreqQuery(int[] arr) {
+    for (int i = 0; i < ids.length; ++i)
+        ids[i] = new ArrayList<>();
+    for (int i = 0; i < arr.length; ++i)
+        ids[arr[i]].add(i);
+}
+public int query(int l, int r, int v) {
+    return lower_bound.apply(Collections.binarySearch(ids[v], r + 1)) - lower_bound.apply(Collections.binarySearch(ids[v], l));
+}
 }
 
 /**
