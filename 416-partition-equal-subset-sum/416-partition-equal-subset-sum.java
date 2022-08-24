63b7ca9a8f1c09aa58b49662ac889b15276c3d72
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-08-24 20:06:54 +0530

    Time: 121 ms (24.70%), Space: 53.8 MB (65.76%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,30 +1,29 @@
 class Solution {
-    Boolean dp[][];
     public boolean canPartition(int[] nums) {
         int sum=0;
-        for(int i:nums)
-        {
-            sum+=i;
-        }
+        for(int i:nums){sum+=i;}
         if(sum%2!=0)
         {return false;}
         sum/=2;
-         dp=new Boolean[nums.length][sum+1];
-        boolean take=helper(1,nums,sum);
-        boolean leave=helper(1,nums,sum);
-        return take|leave;
-    }
-    boolean helper(int idx,int nums[],int sum)
-    {
-        if(idx>=nums.length||sum<=0)
+        boolean dp[][]=new boolean[nums.length][sum+1];
+        for(int i=0;i<nums.length;i++)
+        {dp[i][0]=true;}
+        for(int i=0;i<nums.length;i++)
         {
-            return sum==0;
+            for(int j=1;j<=sum;j++)
+            {
+if(i==0)
+{
+    if(nums[i]==j)
+    {dp[i][j]=true;}
+}
+                else{
+                dp[i][j]=dp[i-1][j];
+                if(j-nums[i]>=0)
+                {dp[i][j]|=dp[i-1][j-nums[i]];}
+            }
         }
-        if(dp[idx][sum]!=null)
-        {return dp[idx][sum];}
-        boolean take=helper(idx+1,nums,sum-nums[idx]);
-        boolean leave=helper(idx+1,nums,sum);
-        dp[idx][sum]=take|leave;
-        return take|leave;
+        }
+        return dp[nums.length-1][sum];
     }
 }
