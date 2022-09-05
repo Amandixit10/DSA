347ec514e6b2b7dec30ebed1966ba49b3a3a784a
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-09-05 13:36:24 +0530

    Time: 15 ms (8.97%), Space: 44.5 MB (79.65%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -1,20 +1,23 @@
 class Solution {
-    Integer dp[][]=new Integer[501][501];
     public int minDistance(String word1, String word2) {
         int n=word1.length();
         int m=word2.length();
-        if(n==0||m==0)
+        int dp[][]=new int[n+1][m+1];
+        for(int i=0;i<=n;i++)
         {
-            return Math.max(n,m);
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0||j==0)
+                {dp[i][j]=Math.max(i,j);
+                continue;}
+                
+                    if(word1.charAt(i-1)==word2.charAt(j-1))
+                    {dp[i][j]=dp[i-1][j-1];}
+                    else{
+                        dp[i][j]=1+Math.min(dp[i][j-1],Math.min(dp[i-1][j-1],dp[i-1][j]));
+                    }
+            }
         }
-        if(dp[n][m]!=null)
-        {return dp[n][m];}
-        if(word1.charAt(0)==word2.charAt(0))
-        {return minDistance(word1.substring(1),word2.substring(1));}
-        int add=1+minDistance(word1,word2.substring(1));
-        int del=1+minDistance(word1.substring(1),word2);
-        int rep=1+minDistance(word1.substring(1),word2.substring(1));
-        dp[n][m]=Math.min(add,Math.min(rep,del));
         return dp[n][m];
     }
 }
