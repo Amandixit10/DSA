a8ab86f904c3c1895ce12746247b38bcf734c340
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-09-19 00:03:53 +0530

    Time: 1 ms (99.76%), Space: 42.8 MB (95.11%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,60 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        // this can be done like , first calculate all left side traps and then calculate all right side traps
-        
-        // calculating right side traps
-        
         int n=height.length;
-        int sum=0;
-        int total=0;
-        int i=-1;
-        int j=-1;
-        for(int x=0;x<n;x++)
+        int pre[]=new int[n];
+        int suf[]=new int[n];
+        pre[0]=height[0];
+        suf[n-1]=height[n-1];
+        for(int i=1;i<n;i++)
         {
-    if(height[x]!=0)
-    {
-    i=x;
-    j=x;
-        break;
-    }
-    }
-if(i==-1)
-{return 0;}
-        while(j<n)
-      {
-        sum+=height[j];
-       if(height[j]>=height[i])
-        {
-           sum-=height[i]+height[j];
-         //  System.out.print(j+"  ");
-  int val=(height[i]*(j-i-1))-sum;
-        //   System.out.println(val);
-           total+=val;
-           sum=height[j];
-           i=j;
-        }     
-            j++;
-     }
-        int idx=i;
-        i=n-1;
-        j=n-1;
-        sum=0;
-        while(j>=idx)
-        {
-         sum+=height[j];
-       if(height[j]>=height[i])
-        {
-           sum-=height[i]+height[j];
-          // System.out.print(j+"  ");
-  int val=(height[i]*(i-j-1))-sum;
-       //    System.out.println(val+" "+sum);
-           total+=val;
-           sum=height[j];
-           i=j;
-        }     
-            j--;   
+            pre[i]=Math.max(pre[i-1],height[i]);
         }
-return total;
-        
+        for(int i=n-2;i>=0;i--)
+        {
+            suf[i]=Math.max(suf[i+1],height[i]);
+        }
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+        count+=Math.min(pre[i],suf[i])-height[i];
+        }
+        return count;
     }
 }
