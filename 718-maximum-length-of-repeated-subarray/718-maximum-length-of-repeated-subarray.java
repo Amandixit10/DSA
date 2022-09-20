447d14053fc86bd1693468a7d035a4b6949f2e9a
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-09-20 22:25:27 +0530

    Time: 135 ms (12.97%), Space: 77.3 MB (69.77%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -2,7 +2,7 @@ class Solution {
     public int findLength(int[] nums1, int[] nums2) {
         int n=nums1.length;
         int m=nums2.length;
-        int dp[][][]=new int[n][m][2];
+        int dp[][]=new int[n][m];
         int max=0;
         for(int i=n-1;i>=0;i--)
         {
@@ -10,23 +10,14 @@ class Solution {
             {
                 if(nums1[i]==nums2[j])
                 {
-                    dp[i][j][1]=1;
+                    dp[i][j]=1;
                     if(i<n-1&&j<m-1)
                     {
-                        dp[i][j][1]+=dp[i+1][j+1][1];
+                        dp[i][j]+=dp[i+1][j+1];
                     }
                 }
-                else{
-                    if(j<m-1)
-                    {
-                    dp[i][j][0]=Math.max(dp[i][j+1][0],dp[i][j+1][1]);
-                    }
-                    if(i<n-1)
-                    {
-                    dp[i][j][0]=Math.max(dp[i][j][0],Math.max(dp[i+1][j][0],dp[i+1][j][1]));
-                    }
-                }
-       max=Math.max(max,Math.max(dp[i][j][0],dp[i][j][1]));
+        
+       max=Math.max(max,dp[i][j]);
             }
         }
         
