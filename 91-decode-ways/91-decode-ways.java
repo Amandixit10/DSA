50d369a21bfea399cb0c9715e71672aebfeb4310
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-10-01 17:41:36 +0530

    Time: 1 ms (98.72%), Space: 40.8 MB (89.52%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,31 +1,36 @@
 class Solution {
-    HashMap<String,Integer> map=new HashMap<>();
+Integer dp[];
     public int numDecodings(String s) {
-        if(s.length()<2)
+        dp=new Integer[s.length()];
+          int ans=helper(0,s);
+        return ans;
+    }
+  int helper(int idx,String s)
+    {
+        if(idx>=s.length())
         {
-            if(s.length()==0)
-            {return 1;}
-            return s.charAt(0)!='0'?1:0;}
-        if(map.containsKey(s))
-        {return map.get(s);}
+            return 1;
+        }
+        if(dp[idx]!=null)
+        {return dp[idx];}
         int cnt=0;
-        if(s.charAt(0)=='0')
+        if(s.charAt(idx)=='0')
         {
             return 0;
         }
-   cnt+=numDecodings(s.substring(1));
-      if(s.length()>1)
+   cnt+=helper(idx+1,s);
+      if(idx+1<s.length())
       {
-      if(s.charAt(0)!='0')
+      if(s.charAt(idx)!='0')
       {
-          int val=Integer.parseInt(s.substring(0,2));
+          int val=Integer.parseInt(s.substring(idx,idx+2));
           if(val>=1&&val<=26)
           {
-         cnt+=numDecodings(s.substring(2));
+         cnt+=helper(idx+2,s);
           }
       }
       }
-        map.put(s,cnt);
+        dp[idx]=cnt;
         return cnt;
     }
 }
