e945d9dd19447ac94a2ce324a60637c32fbe1f32
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-10-01 22:05:50 +0530

    Time: 105 ms (11.11%), Space: 152.3 MB (33.33%) - LeetHub

--- 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.java
@@ -1,20 +1,28 @@
 class LUPrefix {
-
-    int len = 0;
-    boolean isP[];
+boolean is[];
+    int len=0;
     public LUPrefix(int n) {
-        isP = new boolean[n+1];
+        is=new boolean[n+1];
     }
     
     public void upload(int video) {
-        isP[video] = true;
+       is[video]=true; 
     }
     
     public int longest() {
-        for(int i=len+1; i<isP.length; i++){
-            if(isP[i] == false) break;
+        for(int i=len+1;i<is.length;i++)
+        {
+            if(!is[i])
+            {break;}
             len++;
         }
         return len;
     }
 }
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix obj = new LUPrefix(n);
+ * obj.upload(video);
+ * int param_2 = obj.longest();
+ */
