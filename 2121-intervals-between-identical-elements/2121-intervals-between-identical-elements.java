8c4ab35016ed178ce0711f928c1fa9e020448520
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-10-28 19:14:17 +0530

    Time: 455 ms (16.09%), Space: 289.4 MB (9.77%) - LeetHub

--- 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
+++ 2121-intervals-between-identical-elements/2121-intervals-between-identical-elements.java
@@ -1,33 +1,33 @@
 class Solution {
     public long[] getDistances(int[] arr) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        HashMap<Integer,Integer> cnt=new HashMap<>();
-        int n=arr.length;
-        long dp[]=new long[n];
-        long x[]=new long[n];
-        for(int i=0;i<n;i++)
-        {
-            if(map.containsKey(arr[i]))
-            {
-                int val=cnt.get(arr[i]);
-                dp[i]=val*(i-map.get(arr[i]))+dp[map.get(arr[i])];
+        long[] output = new long[arr.length];
+        Map<Integer, Long> sumMap = new HashMap<>();
+        Map<Integer, Integer> countMap = new HashMap<>();
+        for (int i = 0; i < arr.length; ++ i) {
+            if (!sumMap.containsKey(arr[i])) {
+                sumMap.put(arr[i], 0l);
+                countMap.put(arr[i], 0);
             }
-            map.put(arr[i],i);
-            cnt.put(arr[i],cnt.getOrDefault(arr[i],0)+1);
+            
+            output[i] += i * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
+            sumMap.put(arr[i], sumMap.get(arr[i]) + i);
+            countMap.put(arr[i], countMap.get(arr[i]) + 1);
         }
-        map=new HashMap<>();
-        cnt=new HashMap<>();
-        for(int i=n-1;i>=0;i--)
-        {
-            if(map.containsKey(arr[i]))
-            {
-                int val=cnt.get(arr[i]);
-            x[i]+=val*(map.get(arr[i])-i)+x[map.get(arr[i])];
+        
+        sumMap = new HashMap<>();
+        countMap = new HashMap<>();
+        int len = arr.length;
+        for (int i = len - 1; i >= 0; -- i) {
+            if (!sumMap.containsKey(arr[i])) {
+                sumMap.put(arr[i], 0l);
+                countMap.put(arr[i], 0);
             }
-            map.put(arr[i],i);
-            cnt.put(arr[i],cnt.getOrDefault(arr[i],0)+1);
-            dp[i]+=x[i];
+            
+            output[i] += (len - i - 1) * (long)countMap.get(arr[i]) - sumMap.get(arr[i]);
+            sumMap.put(arr[i], sumMap.get(arr[i]) + (len - i - 1));
+            countMap.put(arr[i], countMap.get(arr[i]) + 1);
         }
-        return dp;
+        
+        return output;
     }
 }
