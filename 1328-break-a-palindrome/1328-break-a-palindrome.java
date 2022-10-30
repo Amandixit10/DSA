5caa90515c922fa6f0e2f08e16a0e0435023b78b
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-10-31 01:41:14 +0530

    Time: 154 ms (5.41%), Space: 55.2 MB (5.73%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.java
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -6,26 +6,12 @@ class Solution {
         {return "";}
         for(int i=0;i<n;i++)
         {
+            if(i==(n/2)&&n%2!=0)
+            {continue;}
             String str=palindrome.charAt(i)=='a'?"b":"a";
     String s=palindrome.substring(0,i)+str+palindrome.substring(i+1,n);
-            if(!ispalin(s))
-            {
             set.add(s);
-            }
         }
         return set.first();
-    }
-    boolean ispalin(String s)
-    {
-        int i=0;
-        int j=s.length()-1;
-        while(i<j)
-        {
-            if(s.charAt(i)!=s.charAt(j))
-            {return false;}
-            i++;
-            j--;
-        }
-        return true;
     }
 }
