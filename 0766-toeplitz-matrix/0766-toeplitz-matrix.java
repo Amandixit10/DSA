1c0ecee8bea88f33c31d2102bc4a716c315d8b4e
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-10-31 14:34:50 +0530

    Time: 2 ms (42.33%), Space: 42.1 MB (94.10%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -1,46 +1,21 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
+        HashMap<Integer,Integer> map=new HashMap<>();
         int n=matrix.length;
         int m=matrix[0].length;
-        if(n>=m)
+        for(int i=0;i<n;i++)
         {
-            for(int i=m;i<=n;i++)
+            for(int j=0;j<m;j++)
             {
-            if(!check(i,m,matrix,m))
-            {return false;}
-            }
-        }
-        else {
-          for(int j=n;j<=m;j++)
-            {
-            if(!check(n,j,matrix,n))
-            {return false;}
-            }  
-        }
-        return true;
-    }
-    boolean check(int row,int col,int matrix[][],int fact)
-    {
-        for(int g=col-fact;g<col;g++)
-        {
-            for(int i=row-fact,j=g;j<col;j++,i++)
-            {
-                if(i==row-fact)
-                {continue;}
-                else if(matrix[i][j]!=matrix[i-1][j-1])
+                int val=i-j;
+                if(map.containsKey(val))
                 {
-                    return false;}
-            }
-        }
-           for(int g=row-fact;g<row;g++)
-        {
-            for(int j=col-fact,i=g;i<row;j++,i++)
-            {
-                if(j==col-fact)
-                {continue;}
-                else if(matrix[i][j]!=matrix[i-1][j-1])
-                {
-                    return false;}
+                    if(map.get(val)!=matrix[i][j])
+                    {return false;}
+                }
+                else{
+                    map.put(val,matrix[i][j]);
+                }
             }
         }
         return true;
