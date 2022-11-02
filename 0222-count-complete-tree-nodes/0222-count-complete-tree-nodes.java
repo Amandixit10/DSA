a7799cea2b23ee4f1843bac023382879f3a2c80d
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-11-02 23:28:34 +0530

    Time: 0 ms (100.00%), Space: 49.5 MB (70.52%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,8 +15,15 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
-        if(root==null)
-        {return 0;}
-        return 1+countNodes(root.left)+countNodes(root.right);
+        int h=height(root);
+        if(h<0)
+        {
+            return 0;
+        }
+        return height(root.right)==h-1?(1<<h)+countNodes(root.right):(1<<(h-1))+countNodes(root.left);
+    }
+    int height(TreeNode root)
+    {
+        return root==null?-1:1+height(root.left);
     }
 }
