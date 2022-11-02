130f2a02c6e483b78ce879e08d3ccb4e02f19b3c
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-11-02 13:08:56 +0530

    Time: 0 ms (100.00%), Space: 40 MB (95.19%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.java
@@ -1,42 +1,47 @@
 class Solution {
+    Integer dp[][];
     public int minMutation(String start, String end, String[] bank) {
        int min=Integer.MAX_VALUE;
         int n=bank.length;
-        boolean dp[]=new boolean[n];
+        dp=new Integer[n+1][n+1];
+        boolean fill[]=new boolean[n];
         for(int i=0;i<n;i++)
         {
    if(valid(start,bank[i]))
    {
-       dp[i]=true;
-       int val=helper(bank[i],end,dp,bank);
+       fill[i]=true;
+       int val=helper(bank[i],end,fill,bank,0,i+1);
        if(val!=Integer.MAX_VALUE)
        {
            min=Math.min(min,val+1);
        }
-       dp[i]=false;
+       fill[i]=false;
    }
         }
         return min==Integer.MAX_VALUE?-1:min;
     }
-    int helper(String start,String end,boolean dp[],String bank[])
+    int helper(String start,String end,boolean fill[],String bank[],int prev,int curr)
     {
-        int n=dp.length;
+        int n=fill.length;
         if(start.equals(end))
         {return 0;}
+        if(dp[prev][curr]!=null)
+        {return dp[prev][curr];}
         int min=Integer.MAX_VALUE;
          for(int i=0;i<n;i++)
         {
-   if(!dp[i]&&valid(start,bank[i]))
+   if(!fill[i]&&valid(start,bank[i]))
    {
-       dp[i]=true;
-       int val=helper(bank[i],end,dp,bank);
+       fill[i]=true;
+       int val=helper(bank[i],end,fill,bank,curr,i);
        if(val!=Integer.MAX_VALUE)
        {
            min=Math.min(min,val+1);
        }
-       dp[i]=false;
+       fill[i]=false;
    }
         }
+        dp[prev][curr]=min;
         return min;
     }
 boolean valid(String s,String t)
