99b6278a9164b0382abeef896bc6abbcd72d146e
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-11-03 06:14:51 +0530

    Time: 153 ms (58.24%), Space: 104.7 MB (63.93%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.java
@@ -1,24 +1,35 @@
 class Solution {
-
     public int longestPalindrome(String[] words) {
-        HashMap<String, Integer> map = new HashMap<>();
-        int max = 0;
-        for (int i = 0; i < words.length; i++) {
-            String rev = words[i].charAt(1) + "" + words[i].charAt(0);
-            if (map.containsKey(rev)) {
-                max += 4;
-                map.put(rev, map.get(rev) - 1); // decrease its count as we won't be taking it again
-                if (map.get(rev) == 0) map.remove(rev);
-                continue; // as we also won't be taking current string again
-            }
-            map.put(words[i], map.getOrDefault(words[i], 0) + 1);
+        HashMap<String,Integer> map=new HashMap<>();
+        for(String i:words)
+        {
+            map.put(i,map.getOrDefault(i,0)+1);
         }
-        for (String k : map.keySet()) {
-            if (map.get(k) == 1 && (k.charAt(1) + "" + k.charAt(0)).equals(k)) {
-                max += 2;
-                break;
+        int cnt=0;
+        int odd=0;
+        HashSet<String> set=new HashSet<>();
+        for(String i:words)
+        {
+            if(set.contains(i))
+            {continue;}
+            String s=new StringBuilder(i).reverse().toString();
+            if(s.equals(i))
+            {
+                int val=map.get(i);
+                if(val%2!=0)
+                {
+                    odd=1;}
+                val-=val%2;
+            cnt+=val;
             }
+            else if(map.containsKey(s))
+            {
+                cnt+=Math.min(map.get(i),map.get(s))*2;
+            }
+            set.add(i);
+            set.add(s);
         }
-        return max;
+        cnt+=odd;
+        return cnt*2;
     }
 }
