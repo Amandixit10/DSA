60551b47f354fde32a3f08a44fe24be1a2cb429f
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-11-23 17:25:02 +0530

    Time: 31 ms (16.68%), Space: 47.8 MB (35.29%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,60 +1,18 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
+        Set<String> set=new HashSet<>();
         for(int i=0;i<9;i++)
         {
             for(int j=0;j<9;j++)
             {
-               if(board[i][j]!='.')
-               {
-                 if(!valid(i,j,board))
-                 {
-                     return false;
-                 }
-               }
-            }
-        }
-        for(int i=3;i<=9;i+=3)
-        {
-            for(int j=3;j<=9;j+=3)
-            {
-                if(!squareValid(i,j,board))
+                if(board[i][j]!='.')
                 {
-                    return false;}
-            }
-        }
-        return true;
-    }
-    boolean valid(int row,int col,char ar[][])
-    {
-        int val=0;
-        for(int j=0;j<9;j++)
-        {
-            if(ar[row][j]==ar[row][col])
-            {val++;}
-        }
-        if(val>1)
-        {return false;}
-        val=0;
-        for(int i=0;i<9;i++)
-        {
-            if(ar[i][col]==ar[row][col])
-            {val++;}
-        }
-        return val>1?false:true;
-    }
-    boolean squareValid(int row,int col,char ar[][])
-    {
-        int num[]=new int[10];
-        for(int i=row-3;i<row;i++)
-        {
-            for(int j=col-3;j<col;j++)
-            {
-                if(ar[i][j]!='.')
-                {
-             num[ar[i][j]-'0']++;   
-                if(num[ar[i][j]-'0']>1)
+                int val=board[i][j]-'0';
+                if(!set.add(val+"in row"+i)||
+                   !set.add(val+"in col"+j)||
+                   !set.add(val+"in block"+(i/3+"-"+j/3)))
                 {return false;}
-                }
+            }
             }
         }
         return true;
