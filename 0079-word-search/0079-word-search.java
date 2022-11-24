aa32024002a48428e4aee9fc5bbb685528ce0904
Aman dixit <55151241+Amandixit10@users.noreply.github.com>
2022-11-24 07:02:07 +0530

    Time: 1620 ms (5.01%), Space: 118.4 MB (7.31%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,42 +1,37 @@
 class Solution {
-    boolean visited[][];
     int dir[][]={{1,0},{0,1},{-1,0},{0,-1}};
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
-        visited=new boolean[n][m];
+        boolean visited[][]=new boolean[n][m];
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-           if(dfs(i,j,board,word,0,n,m))
-           {
-               return true;
-           }
+                if(board[i][j]==word.charAt(0))
+                if(find(i,j,board,word,"",visited,0))
+                {
+                    return true;
+                }
             }
         }
-       
         return false;
     }
-    boolean dfs(int row,int col,char ar[][],String s,int idx,int n,int m)
+    boolean find(int r,int c,char board[][],String word,String asf,boolean visited[][],int idx)
     {
-        if(row<0||col<0||row>=n||col>=m||visited[row][col]||ar[row][col]!=s.charAt(idx))
-        {
-            return false;
-        }
-        if(idx>=s.length()-1)
-        {
-            return true;
-        }
-        visited[row][col]=true;
-        for(int i=0;i<4;i++)
-        {
-            if(dfs(row+dir[i][0],col+dir[i][1],ar,s,idx+1,n,m))
-            {
-                return true;
-            }
-        }
-        visited[row][col]=false;
+        if(r<0||c<0||r>=board.length||c>=board[0].length||visited[r][c]||board[r][c]!=word.charAt(idx))
+        {return false;}
+        asf+=word.charAt(idx)+"";
+        idx+=1;
+        if(asf.compareTo(word)==0)
+        {return true;}
+        visited[r][c]=true;
+      for(int i=0;i<4;i++)
+      {
+          if(find(r+dir[i][0],c+dir[i][1],board,word,asf,visited,idx))
+          {return true;}
+      }
+        visited[r][c]=false;
         return false;
     }
 }
